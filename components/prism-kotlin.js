306220471aea7499f7096420f66eede4ed6e535a
Rico Magnucki <rico@icancode.de>
2015-08-09 15:30:22 +0200

    Update prism-kotlin.js

--- components/prism-kotlin.js
+++ components/prism-kotlin.js
@@ -1,8 +1,15 @@
 Prism.languages.kotlin = Prism.languages.extend('clike', {
-	'keyword': /\b(var|val|data|Any|abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|fun)\b/,
+		'class-name': {
+		pattern: /((?:(?:class|data|abstract|interface|extends|implements|trait|instanceof|new)\s+)|(?:catch\s+\())[a-z0-9_\.\\]+/i,
+		lookbehind: true,
+		inside: {
+			punctuation: /(\.|\\)/
+		}
+	},
+	'keyword': /\b(when|vararg|override|open|attribute|inline|data|get|set|Array|Char|Float|Double|Long|Short|Int|String|Unit|Byte|Boolean|Any|as|it|is|typealias|init|constructor|enum|internal|inner|in|out|var|val|data|Any|abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|fun)\b/,
 	'number': /\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp\-]+\b|\b\d*\.?\d+[e]?[\d]*[df]\b|\b\d*\.?\d+\b/i,
 	'operator': {
-		pattern: /(^|[^\.])(?:\+=|\+\+?|-=|--?|!=?|<{1,2}=?|>{1,3}=?|==?|&=|&&?|\|=|\|\|?|\?|\*=?|\/=?|%=?|\^=?|:|~)/m,
+		pattern: /(^|[^\.])(?:\+=|\+\+?|-=|--?|!=?|<{1,2}=?|>{1,3}=?|==?|&=|&&?|\|=|\|\|?|\?|\*=?|\/=?|%=?|\^=?|:|~|::)/m,
 		lookbehind: true
 	}
 });
